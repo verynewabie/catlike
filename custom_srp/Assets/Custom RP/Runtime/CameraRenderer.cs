4ccf12fec0de65d525c2c6c95e8988d5c2e55e63
verynewabie <1159046908@qq.com>
2025-05-01 10:59:36 +0800

    init srp

---
+++ custom_srp/Assets/Custom RP/Runtime/CameraRenderer.cs
@@ -0,0 +1,86 @@
+﻿using UnityEngine;
+using UnityEngine.Rendering;
+
+public partial class CameraRenderer
+{
+    // TODO 目前物体会盖住UI
+    private CommandBuffer buffer = new CommandBuffer();
+    private ScriptableRenderContext context; 
+    private Camera camera;
+    private CullingResults cullingResults;
+    private static ShaderTagId unlitShaderTagId = new ShaderTagId("SRPDefaultUnlit");
+    
+    public void Render(ScriptableRenderContext context, Camera camera)
+    {
+        this.context = context;
+        this.camera = camera;
+        
+        PrepareBuffer();
+        PrepareForSceneWindow();
+        if (!Cull()) {
+            return;
+        }
+        Setup();
+        DrawVisibleGeometry();
+        DrawUnsupportedShaders();
+        DrawGizmos();
+        Submit();
+    }
+    
+    private bool Cull()
+    {
+        if (camera.TryGetCullingParameters(out ScriptableCullingParameters p)) {
+            cullingResults = context.Cull(ref p);
+            return true;
+        }
+        return false;
+    }
+
+    private void Setup()
+    {
+        context.SetupCameraProperties(camera);
+        // 该函数在 Frame Debugger 中会包裹一层 BufferName， 因此放到 BeginSample 前, 此方法放到 SetupCameraProperties 前后时清空方法也不同
+        CameraClearFlags flags = camera.clearFlags;
+        buffer.ClearRenderTarget(
+            flags <= CameraClearFlags.Depth,
+            flags <= CameraClearFlags.Color,
+            flags <= CameraClearFlags.Color ?
+                camera.backgroundColor.linear : Color.clear
+        );
+        buffer.BeginSample(sampleName);
+        ExecuteBuffer();
+    }
+    
+    private void DrawVisibleGeometry()
+    {
+        var sortingSettings = new SortingSettings(camera)
+        {
+            criteria = SortingCriteria.CommonOpaque
+        };
+        var drawingSettings = new DrawingSettings(unlitShaderTagId, sortingSettings);
+        var filteringSettings = new FilteringSettings(RenderQueueRange.opaque);
+        context.DrawRenderers(cullingResults, ref drawingSettings, ref filteringSettings);
+        
+        context.DrawSkybox(camera);
+        
+        sortingSettings.criteria = SortingCriteria.CommonTransparent;
+        drawingSettings.sortingSettings = sortingSettings;
+        filteringSettings.renderQueueRange = RenderQueueRange.transparent;
+
+        context.DrawRenderers(cullingResults, ref drawingSettings, ref filteringSettings);
+    }
+
+    private void Submit()
+    {
+        buffer.EndSample(sampleName);
+        ExecuteBuffer();
+        context.Submit();
+    }
+
+    private void ExecuteBuffer()
+    {
+        context.ExecuteCommandBuffer(buffer);
+        buffer.Clear();
+    }
+
+}
